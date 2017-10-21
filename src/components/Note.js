581da1cd1f65b91d1e8fa01750f46fb39c219d6c
wanghao8080 <117663444@qq.com>
2017-10-21 15:49:56 +0800

    格式化数据的显示

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import Editor from './Editor'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+import _ from 'lodash'
+moment.locale('zh-CN')
 
 class Note extends Component {
   state = {
@@ -8,19 +12,31 @@ class Note extends Component {
     updated: this.props.entity.meta.updated || this.props.entity.meta.created
   }
 
+  updated () {
+    return moment(this.state.updated).fromNow()
+  }
+
+  header () {
+    return _.truncate(this.state.body, { length: 30 }) || '新建笔记'
+  }
+
+  words() {
+    return this.state.body.length
+  }
+
   render () {
     return (
       <div className="item">
         <div className="meta">
-          { this.state.updated }
+          { this.updated() }
         </div>
         <div className="content">
           <div className="header">
-            { this.state.body }
+            { this.header() }
           </div>
           <div className="extra">
             <Editor />
-            { this.state.body.length } 字
+            { this.words() } 字
             <i className="right floated trash outline icon">
 
             </i>
